65e7acb9ff949162adada1cf8a3e53b6abd22097
Artem Zhukov <green.azhukov@gmail.com>
2017-05-23 06:36:37 +0200

    started a Boston Housing Project

--- Lectures/learning_curve.py
+++ Lectures/learning_curve.py
@@ -1,3 +1,21 @@
+import pandas as pd
 from sklearn.model_selection import learning_curve
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
+
+X = pd.read_csv('titanic_data.csv')
+X = X._get_numeric_data()
+y = X['Survived']
+del X['Age'], X['Survived']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y)
 
 
+
+clf2 = GaussianNB()
+clf2.fit(X_train, y_train)
+
+cv = None
+n_jobs = 5
+
+print learning_curve(GaussianNB(), X, y, cv=cv, n_jobs=n_jobs)
--- Lectures/mean_abs_error.py
+++ Lectures/mean_abs_error.py
@@ -1,8 +1,9 @@
 # Load the dataset
 import numpy as np
 
-from sklearn import cross_validation
+from sklearn.model_selection import train_test_split
 from sklearn.datasets import load_linnerud
+from sklearn.model_selection import learning_curve
 
 linnerud_data = load_linnerud()
 X = linnerud_data.data
@@ -16,7 +17,7 @@ from sklearn.linear_model import LinearRegression
 # using the standard settings for train_test_split.
 # Then, train and test the classifiers with your newly split data instead of X and y.
 seed = np.random.seed(5)
-X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, random_state=seed)
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=seed)
 reg1 = DecisionTreeRegressor()
 reg1.fit(X_train, y_train)
 print "Decision Tree mean absolute error: {:.2f}".format(mae(y_test, reg1.predict(X_test)))
@@ -25,7 +26,4 @@ reg2 = LinearRegression()
 reg2.fit(X_train, y_train)
 print "Linear regression mean absolute error: {:.2f}".format(mae(y_test, reg2.predict(X_test)))
 
-results = {
-    "Linear Regression": 10.33,
-    "Decision Tree": 11.4
-}
+learning_curve(LinearRegression(), X, y, cv=train_test_split(), train_sizes=20)
