3cdef70559c52e4d5a58f44a591834892be4046d
Artem Zhukov <green.azhukov@gmail.com>
2017-06-15 14:07:07 +0200

    update

--- Lectures/DT.py
+++ Lectures/DT.py
@@ -6,3 +6,32 @@ def classify(features_train, labels_train):
     clf = DecisionTreeClassifier()
     clf = clf.fit(features_train, labels_train)
     return clf
+
+
+import sys
+from class_vis import prettyPicture
+from prep_terrain_data import makeTerrainData
+from sklearn.metrics import accuracy_score
+from sklearn.tree import DecisionTreeClassifier
+import numpy as np
+import pylab as pl
+
+features_train, labels_train, features_test, labels_test = makeTerrainData()
+
+#################################################################################
+
+
+########################## DECISION TREE #################################
+
+
+
+#### your code goes here
+clf = DecisionTreeClassifier()
+clf = clf.fit(features_train, labels_train)
+
+### be sure to compute the accuracy on the test set
+acc = accuracy_score(labels_test, clf.predict(features_test))
+
+
+def submitAccuracies():
+    return {"acc": round(acc, 3)}
